68e88cec019d0d6c0fda3d1899b4e7c2aa757760
AlexisFlach <alexis.flach87@gmail.com>
2021-04-26 16:46:53 +0200

    make it better

--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-
+const path = require('path')
 const app = express();
 
 const pool = require('./db')
@@ -37,8 +37,14 @@ app.put("/api/birds/:id", async (req, res) => {
   }
 })
 
+if(process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'))
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  })
+}
 
 
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 app.listen(port, () => `Server running on port ${port}`);
